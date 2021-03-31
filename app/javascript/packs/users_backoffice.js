2ed04bb84f224da3dcad7836067ffeea1c5ff18d
Henrique <ch.henriquevinha@gmail.com>
2021-03-30 22:49:17 -0300

    Add bootstrap, jquery and use templates - Backoffices Ok

---
+++ app/javascript/packs/users_backoffice.js
@@ -0,0 +1,15 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+
+//= require '../../../vendor/assets/javascripts/jquery-2.2.3'
+import   'bootstrap/dist/js/bootstrap'
+import   'fastclick/lib/fastclick'
+import   'nprogress/nprogress'
+import   'malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar'
+import '../../../lib/assets/javascripts/custom'
+
+
+console.log('deu certo jquery 2 2 3')
